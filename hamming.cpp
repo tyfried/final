4424feabd737bcc6ddfdb59b4176ac5b7b514290
tyfried <tyfried@umich.edu>
2015-04-23 13:39:57 -0400

    progress maybe

---
+++ hamming.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdint>
+
+using namespace std;
+
+int popcount_1(uint64_t x);
+uint wegner(uint input);
+
+const uint64_t m1  = 0x5555555555555555; //binary: 0101...
+const uint64_t m2  = 0x3333333333333333; //binary: 00110011..
+const uint64_t m4  = 0x0f0f0f0f0f0f0f0f; //binary:  4 zeros,  4 ones ...
+const uint64_t m8  = 0x00ff00ff00ff00ff; //binary:  8 zeros,  8 ones ...
+const uint64_t m16 = 0x0000ffff0000ffff; //binary: 16 zeros, 16 ones ...
+const uint64_t m32 = 0x00000000ffffffff; //binary: 32 zeros, 32 ones
+const uint64_t hff = 0xffffffffffffffff; //binary: all ones
+const uint64_t h01 = 0x0101010101010101; //the sum of 256 to the power of 0,1,2,3...
+
+
+
+int main(){
+	uint64_t x;
+	while(1){
+		cin >> x;
+		cout << popcount_1(x) << endl;
+		cout << wegner(x) << endl;
+	}
+	
+	return 0;
+}
+
+
+int popcount_1(uint64_t x) {
+    x = (x & m1 ) + ((x >>  1) & m1 ); //put count of each  2 bits into those  2 bits 
+    x = (x & m2 ) + ((x >>  2) & m2 ); //put count of each  4 bits into those  4 bits 
+    x = (x & m4 ) + ((x >>  4) & m4 ); //put count of each  8 bits into those  8 bits 
+    x = (x & m8 ) + ((x >>  8) & m8 ); //put count of each 16 bits into those 16 bits 
+    x = (x & m16) + ((x >> 16) & m16); //put count of each 32 bits into those 32 bits 
+    x = (x & m32) + ((x >> 32) & m32); //put count of each 64 bits into those 64 bits 
+    return x;
+}
+
+uint wegner(uint input)
+        {
+            uint count;
+            for (count = 0; input!=0; count++)
+            {
+                input &= input - 1; // turn off the rightmost 1-bit
+            }
+            return count;
+        }
---
+++ sphere_packing_solo.m
@@ -0,0 +1,39 @@
+syms k;
+for q = 2:5
+    r = 1-1/q;
+    %data_sp_gr = zeros(100000,3);
+    data_plt = zeros(100000,3);
+ 
+    %count_sp = 1;
+    count_plt = 1;
+    
+   for n = 8:25
+       for d=n/2:n
+           t = floor((d-1)/2);
+           sp = (q^n)/symsum(nchoosek(n,k)*(q-1)^k,0,t);
+           
+           pltkn = d/(d-r*n);
+           data_plt(count_plt,1) = n;
+           data_plt(count_plt,2) = d;
+           data_plt(count_plt,3) = sp;
+           count_plt=count_plt+1;
+       end
+   end
+    
+   tri = delaunay(data_plt(1:count_plt,1),data_plt(1:count_plt,2));
+   
+   figure;
+   trisurf(tri,data_plt(1:count_plt,1),data_plt(1:count_plt,2),data_plt(1:count_plt,3));
+   axis vis3d
+   
+%    figure;
+%    scatter(data_sp_gr(1:count_sp,1),data_sp_gr(1:count_sp,2),'g')
+%    hold on
+%    scatter(data_plt(1:count_plt,1),data_plt(1:count_plt,2),'b')
+%    hold on
+%    scatter(data_sp_gr(1:count_sp,1),data_sp_gr(1:count_sp,3),'r')
+%    title(['q = ',num2str(q)])
+%    legend('Sphere Packing','Plotkin','Greismer','Location','southwest');
+%    xlabel('Relative Distance (d/n)');
+%    ylabel('Bound');
+end
